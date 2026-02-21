203307975cab18eb0f7de9b03ae2d253da574246
yengfuan <fengyuan3426@gmail.com>
2026-02-21 20:35:35 +0800

    Work with streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -4,6 +4,7 @@ import se.edu.streamdemo.data.Datamanager;
 import se.edu.streamdemo.task.Deadline;
 import se.edu.streamdemo.task.Task;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Main {
@@ -15,10 +16,13 @@ public class Main {
 
         System.out.println("Printing all data ...");
         printAllData(tasksData);
+        printDataUsingStreams(tasksData);
 
         System.out.println("Printing deadlines ...");
-        printDeadlines(tasksData);
+//        printDeadlines(tasksData);
+        printDeadlineUsingStreams(tasksData);
 
+        System.out.println("Total number of deadlines: (streams)" + countDeadlinesUsingStreams(tasksData));
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
     }
@@ -37,10 +41,23 @@ public class Main {
         return count;
     }
 
+    public static void printDeadlineUsingStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing deadline using stream");
+        tasks.parallelStream()
+                .filter((Task t) -> t instanceof Deadline)
+                .forEach(System.out::println);
+    }
+
     public static void printAllData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printDataUsingStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using streams");
+        tasks.stream()
+                .forEach(System.out::println);
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
@@ -49,6 +66,13 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    private static int countDeadlinesUsingStreams(ArrayList<Task> tasks) {
+        int count = (int)tasks.stream()
+                .filter(((Task t) -> t instanceof Deadline))
+                .count();
+        return count;
     }
 
 }
